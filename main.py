c53fc3e4bbdec20bf2b9d84797f53e01763f0200
Olivia Gonzalez <Olivia Gonzalez>
2016-09-10 11:35:02 -0500

    created initial implemetation following asciichan example

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                        autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    blogPost = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainPage(Handler):
+    def render_front(self, title="", blogPost="", error=""):
+        blogPosts = db.GqlQuery("SELECT * FROM BlogPost "
+                           "ORDER BY created DESC")
+        self.render("front.html", title=title, blogPost=blogPost, error=error, blogPosts=blogPosts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    def post(self):
+        title = self.request.get("title")
+        blogPost = self.request.get("blogPost")
+
+        if title and blogPost:
+            a = BlogPost(title = title, blogPost = blogPost)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both a title and a post!"
+            self.render_front(title, blogPost,error)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
